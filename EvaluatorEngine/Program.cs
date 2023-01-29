53fcadf2be1ac5310884d0631ce7d0df8493b30d
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2023-01-29 13:09:37 +0100

    Add Evaluator

---
+++ EvaluatorEngine/Program.cs
@@ -0,0 +1,11 @@
+﻿using TDDPractices.Katas;
+
+#region FizzBuzzer evaluator
+
+var fizzBuzzer = new FizzBuzzer();
+
+for (int i = 0; i < 100; i++)
+{
+    Console.WriteLine( fizzBuzzer.GetValue(i));
+}
+#endregion
