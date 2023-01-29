3c4c3bb1aca9146825137a3f07dc9bd01805da22
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2023-01-29 11:51:49 +0100

    Buzzer_WhenNumberIsMultiplesOf3_ReturnsFizz -> FAIL

--- TDDPractices.Tests/FizzBuzzerTests.cs
+++ TDDPractices.Tests/FizzBuzzerTests.cs
@@ -12,11 +12,31 @@ public class FizzBuzzerTests
 	{
 		// Arrange
 		var buzzer = new FizzBuzzer();
+		string expectedOutPut = inputNumber.ToString();
 
 		// Act
-		string expectedNumber = buzzer.GetValue(inputNumber);
+		string actualOutPut = buzzer.GetValue(inputNumber);
 
 		// Assert
-		Assert.Equal(expectedNumber, inputNumber.ToString());
+		Assert.Equal(expectedOutPut, actualOutPut);
+	}
+
+	[Theory]
+	[InlineData(3)]
+	[InlineData(6)]
+	[InlineData(9)]
+	[InlineData(18)]
+	public void Buzzer_WhenNumberIsMultiplesOf3_ReturnsFizz(int inputNumber)
+	{
+		// Arrange
+		var buzzer = new FizzBuzzer();
+		string expectedOutPut = "Fizz";
+
+		// Act
+		var actualOutPut = buzzer.GetValue(inputNumber);
+
+		// Assert
+		Assert.Equal(expectedOutPut, actualOutPut);
+	
 	}
 }
